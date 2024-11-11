95fef793a126d13c856002db6353652cc708b586
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-11-11 03:28:35 +0000

    Add theme colors

--- frontend/src/core/theme/colors.tsx
+++ frontend/src/core/theme/colors.tsx
@@ -1,1 +1,16 @@
 export const bannerColor = "bg-violet-950";
+
+export const lightColors = {
+  primary: "bg-violet-950",
+  // secondary: '#7f7f7f',
+  // background: '#efefef',
+  // border: '#303030',
+};
+
+export const darkColors = {
+  primary: "bg-red-900",
+  // secondary: '#7f7f7f',
+  // background: '#101010',
+  // border: '#b0b0b0',
+};
+
