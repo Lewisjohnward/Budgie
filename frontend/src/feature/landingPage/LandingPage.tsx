0dd766c5630c0b61e9822384eefb9b1c168c9628
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-11-11 02:26:27 +0000

    Style TopBar

--- frontend/src/feature/landingPage/LandingPage.tsx
+++ frontend/src/feature/landingPage/LandingPage.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function LandingPage() {
   return (
     <div className="">
@@ -11,13 +13,14 @@ function TopBar() {
   return (
     <div className="flex justify-between items-center px-[480px] py-4 bg-violet-950">
       <h1 className="text-2xl text-white font-bold">Budgie.</h1>
-      {/* <h1 className="text-white">What is E</h1> */}
-      <div className="space-x-10">
-        <button className="text-white hover:underline">Log In</button>
-        <button className="px-4 py-2 bg-lime-400 text-sky-900 rounded-md hover:bg-lime-500">
-          Star Your Free Trial
-        </button>
-      </div>
+      <ul className="flex items-center space-x-10">
+        <li className="text-white hover:cursor-pointer hover:underline">
+          <Link to={`login`}>Log In</Link>
+        </li>
+        <li className="px-4 py-2 bg-lime-400 text-sky-900 rounded-md hover:cursor-pointer hover:bg-lime-500">
+          <Link to={`login`}>Start Your Free Trial</Link>
+        </li>
+      </ul>
     </div>
   );
 }
