72006903e4f84b76da2c9b27f2bbbd8f272d3c72
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-11-11 02:15:07 +0000

    Extract CreateBrowserRouter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,6 @@
 /* existing imports */
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { BudgetPage, LandingPage, LoginPage } from "./feature";
-import { ErrorPage } from "./components";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <LandingPage />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/login",
-    element: <LoginPage />,
-  },
-  {
-    path: "/budget",
-    element: <BudgetPage />,
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import { router } from "./core";
 
 export default function App() {
   return (
---
+++ frontend/src/core/router.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import { BudgetPage, LandingPage, LoginPage } from "../feature";
+import { ErrorPage } from "../components";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/budget",
+    element: <BudgetPage />,
+  },
+]);
+
+export default router;
