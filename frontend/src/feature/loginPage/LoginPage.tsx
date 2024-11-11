c381de3f4416c28fdb73454e1465e0ac97239fc2
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-11-11 02:26:44 +0000

    Add txt

--- frontend/src/feature/loginPage/LoginPage.tsx
+++ frontend/src/feature/loginPage/LoginPage.tsx
@@ -1,7 +1,8 @@
 export default function LoginPage() {
   return (
-    <>
-      <h1>Login page</h1>
-    </>
+    <div className="h-screen w-screen p-10 bg-[radial-gradient(rgba(53,87,129)_0%,rgba(28,65,72,1)_100%)]">
+      <h1 className="text-2xl text-white font-bold">Budgie.</h1>
+      <p>Budgie aims to help thousands discover how to spend wisely, save confidently, and live joyfully through a straightforward set of transformative habits</p>
+    </div>
   );
 }
