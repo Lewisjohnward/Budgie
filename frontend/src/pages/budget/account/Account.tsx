6d71358c510d6ea7c77e4f1e03cce6f7ff40d4d6
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-11-29 13:07:06 +0000

    Create Account component

--- frontend/src/pages/budget/account/Account.tsx
+++ frontend/src/pages/budget/account/Account.tsx
@@ -1,10 +1,66 @@
-import { useParams } from "react-router-dom";
+import { mockAccounts } from "@/mockData";
+import { useLocation, useParams } from "react-router-dom";
+
+type Transaction = {
+  id: string;
+  accountId: string;
+  categoryId: string;
+  budgetId: string;
+  date: string;
+  payee: string;
+  memo: string;
+  outflow: string;
+  inflow: string;
+  cleared: boolean;
+  // TODO: don't need these two
+  createdAt: string;
+  updatedAt: string;
+};
+
+type Account = {
+  name: string;
+  clearedBalance?: number;
+  unclearedBalance?: number;
+  transactions: Transaction[];
+};
 
 export function Account() {
   const { accountId } = useParams();
+  const location = useLocation();
+  console.log(location.pathname);
+
+  let transactions: Transaction[] = [];
+  let account: Account;
+
+  if (accountId) {
+    const foundAccount = mockAccounts.find((acc) => acc.id === accountId);
+    if (!foundAccount) {
+      return <h1>Account not found</h1>;
+    }
+
+    account = {
+      name: foundAccount.name,
+      clearedBalance: 0,
+      unclearedBalance: 0,
+      transactions: foundAccount.transactions,
+    };
+  } else if (location.pathname === "/budget/account/all") {
+    transactions = mockAccounts.flatMap((account) => account.transactions);
+
+    account = {
+      name: "All accounts",
+      clearedBalance: 0,
+      unclearedBalance: 0,
+      transactions,
+    };
+  }
+
   return (
-    <div>
-      <p>showing account: {accountId}</p>
-    </div>
+    <>
+      <div className="p-4">
+        <div className="font-bold text-2xl tracking-tight">{account!.name}</div>
+      </div>
+      <div className="w-full h-[1px] bg-black/20" />
+    </>
   );
 }
