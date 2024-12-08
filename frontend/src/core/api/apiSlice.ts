519909409fe78072eb941724c27a7d47f6348f83
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-12-08 14:03:12 +0000

    Handle 401 response

--- frontend/src/core/api/apiSlice.ts
+++ frontend/src/core/api/apiSlice.ts
@@ -19,6 +19,26 @@ const baseQuery = fetchBaseQuery({
     }
     return headers;
   },
+  responseHandler: async (response) => {
+    const contentType = response.headers.get("content-type");
+
+    if (response.status === 401) {
+      return "No refresh token";
+    }
+
+    if (contentType?.includes("application/json")) {
+      console.log("json");
+      return response.json(); // Parse as JSON if Content-Type is JSON
+    } else if (contentType?.includes("text/plain")) {
+      console.log("text");
+      return response.text(); // Parse as plain text if Content-Type is text
+    } else if (response.status === 204) {
+      console.log("error 204");
+      return null; // Handle 204 No Content (empty response body)
+    } else {
+      throw new Error(`Unsupported content type: ${contentType}`);
+    }
+  },
 });
 
 const baseQueryWithReauth: BaseQueryFn<
@@ -26,20 +46,25 @@ const baseQueryWithReauth: BaseQueryFn<
   unknown,
   FetchBaseQueryError
 > = async (args, api, extraOptions) => {
+  console.log("base query with reauth");
   let result = await baseQuery(args, api, extraOptions);
 
   // If server returns a 403 when sending a request refresh token
-  if (result.error && result.error.status === 403) {
+  if (result?.error?.status === 403) {
+    console.log("resulst.error && status === 403");
     const refreshResult = await baseQuery("/refresh", api, extraOptions);
     if (refreshResult.data) {
       const email = (api.getState() as RootState).auth.email;
       api.dispatch(setCredentials({ ...refreshResult.data, email }));
       // Retry original query
       result = await baseQuery(args, api, extraOptions);
-  } else {
-    api.dispatch(logOut());
+    } else {
+      api.dispatch(logOut());
+    }
   }
-  }
+
+  console.log("base query, no error");
+
   return result;
 };
 
