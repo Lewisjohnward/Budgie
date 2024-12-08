7678a86bcc693650dae7049963848b09f0f21c49
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-12-08 14:03:39 +0000

    Add login/logout routes

--- frontend/src/core/api/authApiSlice.ts
+++ frontend/src/core/api/authApiSlice.ts
@@ -2,14 +2,28 @@ import { apiSlice } from "./apiSlice";
 
 export const authApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
+    // TODO: this needs typing <res, req>
     login: builder.mutation({
       query: (credentials) => ({
-        url: "/auth",
+        url: "user/login",
         method: "POST",
         body: { ...credentials },
+      }),
+    }),
+    logout: builder.mutation<void, void>({
+      query: () => ({
+        url: "user/logout",
+        method: "POST",
+      }),
+    }),
+    refreshToken: builder.mutation<void, void>({
+      query: () => ({
+        url: "user/refresh",
+        method: "POST",
       }),
     }),
   }),
 });
 
-export const { useLoginMutation } = authApiSlice;
+export const { useLoginMutation, useLogoutMutation, useRefreshTokenMutation } =
+  authApiSlice;
