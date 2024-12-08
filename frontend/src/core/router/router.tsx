e775a3bf97d50f2b8da02de765c72dbb3b59e168
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-12-08 15:01:40 +0000

    When refreshing budget page don't push user to /

--- frontend/src/core/router/router.tsx
+++ frontend/src/core/router/router.tsx
@@ -21,74 +21,66 @@ const router = createBrowserRouter([
     errorElement: <NotFoundPage />,
     children: [
       {
-        path: "/",
+        path: "",
         element: <LandingPage />,
       },
+      {
+        path: "budget",
+        element: (
+          <Auth required>
+            <BudgetPage />
+          </Auth>
+        ),
+        children: [
+          {
+            path: "",
+            element: <Navigate to="allocation" replace />,
+          },
+          {
+            path: "*",
+            element: <Navigate to="allocation" replace />,
+          },
+          {
+            path: "allocation",
+            element: <Allocation />,
+          },
+          {
+            path: "reflect",
+            element: <Reflect />,
+          },
+          {
+            path: "account/all",
+            element: <Account />,
+          },
+          {
+            path: "account/:accountId",
+            element: <Account />,
+          },
+        ],
+      },
+      {
+        path: "user",
+        element: (
+          <Auth>
+            <Outlet />
+          </Auth>
+        ),
+        children: [
+          {
+            path: "login",
+            element: <LoginPage />,
+          },
+          {
+            path: "forgotpassword",
+            element: <ForgotPasswordPage />,
+          },
+          {
+            path: "signup",
+            element: <Signup />,
+          },
+        ],
+      },
     ],
-  },
-  {
-    path: "/users",
-    element: (
-      <Auth>
-        <Outlet />
-      </Auth>
-    ),
-    children: [
-      {
-        path: "login",
-        element: <LoginPage />,
-      },
-      {
-        path: "forgotpassword",
-        element: <ForgotPasswordPage />,
-      },
-      {
-        path: "signup",
-        element: <Signup />,
-      },
-    ],
-  },
-  {
-    path: "/budget",
-    element: (
-      <Auth required>
-        <BudgetPage />
-      </Auth>
-    ),
-    children: [
-      {
-        path: "",
-        element: <Navigate to="allocation" replace />,
-      },
-      {
-        path: "*",
-        element: <Navigate to="allocation" replace />,
-      },
-      {
-        path: "allocation",
-        element: <Allocation />,
-      },
-      {
-        path: "reflect",
-        element: <Reflect />,
-      },
-      {
-        path: "account/all",
-        element: <Account />,
-      },
-      {
-        path: "account/:accountId",
-        element: <Account />,
-      },
-    ],
-  },
-  {
-    path: "/settings",
-    element: (
-      <Auth required>
-        <AccountSettingsPage />,
-      </Auth>
-    ),
   },
 ]);
 
