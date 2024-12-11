d7b2c4b47a3386524964a2bb42af0738b64e7acf
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-12-11 15:08:46 +0000

    Create createStore fn

--- frontend/src/core/store/store.tsx
+++ frontend/src/core/store/store.tsx
@@ -2,16 +2,20 @@ import { configureStore } from "@reduxjs/toolkit/react";
 import authReducer from "../auth/authSlice";
 import { apiSlice } from "../api/apiSlice";
 
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    [apiSlice.reducerPath]: apiSlice.reducer,
-  },
-  middleware: (getDefaultMiddleWare) => 
-    getDefaultMiddleWare().concat(apiSlice.middleware),
-  // TODO: make env variable
-  devTools: true,
-});
+export const createStore = () =>
+  configureStore({
+    reducer: {
+      auth: authReducer,
+      [apiSlice.reducerPath]: apiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleWare) =>
+      getDefaultMiddleWare().concat(apiSlice.middleware),
+    // TODO: make env variable
+    devTools: true,
+  });
+
+export const store = createStore()
+
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
