8ea761d0038469da096ba96f67de9f4ba6c66914
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-12-11 21:08:49 +0000

    Refactor

--- frontend/src/pages/budget/navBar/components/MenuButton.tsx
+++ frontend/src/pages/budget/navBar/components/MenuButton.tsx
@@ -1,16 +1,19 @@
-import clsx from "clsx";
 import { forwardRef } from "react";
+import clsx from "clsx";
+import { selectCurrentUser } from "@/core/auth/authSlice";
 import { BirdIcon, ChevronDownIcon } from "@/core/icons/icons";
 import useMouseOver from "@/core/hooks/useMouseOver";
+import { useAppSelector } from "@/core/hooks/reduxHooks";
 
 type MenuBottonProps = {
-  animate: boolean;
+  animate?: boolean;
   displayText: boolean;
 };
 
 export const MenuButton = forwardRef<HTMLButtonElement, MenuBottonProps>(
   ({ displayText, animate, ...props }, ref) => {
     const { mouseOver, handleMouseOver } = useMouseOver();
+    const email = useAppSelector(selectCurrentUser);
 
     return (
       <button
@@ -19,21 +22,27 @@ export const MenuButton = forwardRef<HTMLButtonElement, MenuBottonProps>(
         ref={ref}
         {...props}
       >
-        <div className="flex flex-row items-center gap-2">
+        <div className="flex-1 flex flex-row items-center gap-2">
           <BirdIcon
             className={clsx(
               animate || mouseOver ? "animate-shake" : "",
               "h-8 w-8",
             )}
+            data-testid="icon"
           />
           {displayText && (
-            <div>
-              <p className="text-left font-bold">budget</p>
-              <p className="text-xs text-white/70">placeholder@email.com</p>
+            <div
+              className="flex-1 flex items-center space-between"
+              data-testid="expanded-info"
+            >
+              <div className="flex-1 flex flex-col text-left">
+                <p className="font-bold">budget</p>
+                <p className="text-xs text-white/70">{email}</p>
+              </div>
+              <ChevronDownIcon />
             </div>
           )}
         </div>
-        {displayText && <ChevronDownIcon />}
       </button>
     );
   },
