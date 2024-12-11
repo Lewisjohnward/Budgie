150af4058433635160a7f6cea5c9f0ce8ad42cd6
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-12-11 21:08:56 +0000

    Create tests

---
+++ frontend/src/pages/budget/navBar/components/MenuButton.test.tsx
@@ -0,0 +1,106 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { configureStore } from "@reduxjs/toolkit/react";
+import { Provider } from "react-redux";
+
+import { MenuButton } from "./MenuButton";
+import authReducer, { AuthState } from "@/core/auth/authSlice";
+
+describe("MenuButton Component", () => {
+  const renderWithProviders = (
+    ui: React.ReactNode,
+    initialAuthState: AuthState,
+  ) => {
+    const store = createMockStore(initialAuthState);
+    return render(<Provider store={store}>{ui}</Provider>);
+  };
+
+  const renderComponent = ({
+    displayText,
+    animate = true,
+    authState,
+  }: {
+    displayText: boolean;
+    animate?: boolean;
+    authState: AuthState;
+  }) => {
+    renderWithProviders(
+      <MenuButton displayText={displayText} animate={animate || false} />,
+      authState,
+    );
+    return {
+      email: screen.queryByText(/test@test.com/i),
+      expandedInfo: screen.queryByTestId("expanded-info"),
+      icon: screen.queryByTestId("icon"),
+    };
+  };
+
+  const createMockStore = (initialAuthState: AuthState) =>
+    configureStore({
+      reducer: {
+        auth: authReducer,
+      },
+      preloadedState: {
+        auth: initialAuthState,
+      },
+    });
+
+  const loggedInAuthState = {
+    email: "test@test.com",
+    token: null,
+  };
+
+  const loggedOutAuthState = {
+    email: null,
+    token: null,
+  };
+
+  it("Renders component", () => {
+    const { expandedInfo } = renderComponent({
+      displayText: true,
+      authState: loggedInAuthState,
+    });
+
+    expect(expandedInfo).toBeInTheDocument();
+  });
+
+  it("Displays email address when logged in", () => {
+    const { expandedInfo, email } = renderComponent({
+      displayText: true,
+      authState: loggedInAuthState,
+    });
+
+    expect(expandedInfo).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  });
+
+  it("Displays no text when logged out", () => {
+    const { email } = renderComponent({
+      displayText: true,
+      authState: loggedOutAuthState,
+    });
+
+    expect(email).not.toBeInTheDocument();
+  });
+
+  it("Does not display expanded info when displayText is false", () => {
+    const { expandedInfo } = renderComponent({
+      displayText: false,
+      authState: loggedOutAuthState,
+    });
+
+    expect(expandedInfo).not.toBeInTheDocument();
+  });
+
+  it("Applies animation class when mouse is over button", () => {
+    renderComponent({
+      displayText: false,
+      authState: loggedOutAuthState,
+    });
+
+    const button = screen.getByRole("button");
+    fireEvent.mouseEnter(button);
+
+    const icon = screen.getByTestId("icon");
+    expect(icon).toHaveClass("animate-shake");
+  });
+});
