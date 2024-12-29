60f4f93b1330aeed104ea02c338718a53a75dbd7
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-12-29 16:38:21 +0000

    getAccounts test

--- backend/src/controllers/BudgetController.test.ts
+++ backend/src/controllers/BudgetController.test.ts
@@ -1,7 +1,12 @@
 import request from "supertest";
-import app from "../app"; // Import your app
 import { PrismaClient } from "@prisma/client"; // Added Prisma imports
-import { insertTransaction, isValidAccount, isValidCategory } from "../utility";
+import app from "../app"; // Import your app
+import {
+  insertTransaction,
+  selectAccounts,
+  userOwnsAccount,
+  validateAccount,
+} from "../utility";
 
 jest.mock("@prisma/client", () => {
   const actualPrisma = jest.requireActual("@prisma/client");
@@ -21,23 +26,28 @@ jest.mock("@prisma/client", () => {
   };
 });
 
-jest.mock("../utility", () => ({
-  ValidateSignature: jest.fn((req) => {
-    req.user = {
-      _id: "dbfbbeb4-89d9-4b08-b627-1be5b4748107",
-      email: "test@test.com",
-    };
-    return true;
-  }),
-  isValidAccount: jest.fn(),
-  isValidCategory: jest.fn(),
-  insertTransaction: jest.fn(),
-}));
+jest.mock("../utility", () => {
+  const actualModule = jest.requireActual("../utility");
+  return {
+    ValidateSignature: jest.fn((req) => {
+      req.user = {
+        _id: "dbfbbeb4-89d9-4b08-b627-1be5b4748107",
+        email: "test@test.com",
+      };
+      return true;
+    }),
+    insertTransaction: jest.fn(),
+    userOwnsAccount: jest.fn(),
+    selectAccounts: jest.fn(),
+    normalizeData: jest.fn(),
+    validateAccount: actualModule.validateAccount,
+  };
+});
 
 const prisma = new PrismaClient();
 
 describe("Budget Controller", () => {
-  describe("add Account", () => {
+  describe("Add account", () => {
     it("should return 400 for missing required fields", async () => {
       const response = await request(app)
         .post("/budget/account")
@@ -111,47 +121,72 @@ describe("Budget Controller", () => {
     });
   });
 
+  describe("getAccounts", () => {
+    it("Should return 500 if there is an error", async () => {
+      (selectAccounts as jest.Mock).mockRejectedValue(
+        new Error("Database error"),
+      );
+      const response = await request(app).get("/budget/account");
+      expect(response.status).toBe(500);
+    });
+
+    it("Should return 200 if there is no error", async () => {
+      const selectAccountsMock = selectAccounts as jest.Mock;
+      selectAccountsMock.mockResolvedValue([]);
+
+      const response = await request(app).get("/budget/account");
+
+      expect(response.status).toBe(200);
+    });
+  });
+
   describe("Add transaction", () => {
-    // date,
-    // outflow,
-    // inflow,
-    // payee,
-    // memo
 
     it("Should return 400 when both inflow and outflow missing", async () => {
       const response = await request(app)
         .post("/budget/transaction")
-        .send({ date: "10-10-2024" })
-        .set("Authorization", "Bearer mock-token");
+        .send({ date: "10-10-2024" });
+
+      expect(response.status).toBe(400);
+    });
+
+    it("Should return 400 when both inflow and outflow provided", async () => {
+      const response = await request(app)
+        .post("/budget/transaction")
+        .send({ date: "10-10-2024", inflow: 50, outflow: 200 });
 
       expect(response.status).toBe(400);
     });
 
     it("Should return 400 if transactionSchema validation fails", async () => {
-      (isValidAccount as jest.Mock).mockReturnValue({ id: "5" });
-      (isValidCategory as jest.Mock).mockReturnValue({ id: "5" });
-
       const response = await request(app).post("/budget/transaction").send({
         inflow: "not-a-number",
       });
       expect(response.status).toBe(400);
     });
 
-    it("Should return 200 if correct data sent", async () => {
-      (isValidAccount as jest.Mock).mockReturnValue({
-        id: "a3e1f9f0-09e2-4d94-8c89-12b50f8d8f2e",
-      });
-
-      (isValidCategory as jest.Mock).mockReturnValue({
-        id: "7c5a7df3-bd02-4576-b9e5-c2c8d6cf4d21",
-      });
+    it("Should return 503 if insertion in db fails", async () => {
+      (insertTransaction as jest.Mock).mockRejectedValue("Failed");
 
       const response = await request(app).post("/budget/transaction").send({
+        accountId: "7c5a7df3-bd02-4576-b9e5-c2c8d6cf4d21",
+        categoryId: "7c5a7df3-bd02-4576-b9e5-c2c8d6cf4d21",
+        inflow: 120,
+      });
+      expect(response.status).toBe(503);
+      (insertTransaction as jest.Mock).mockReset();
+    });
+
+    it("Should return 200 if correct data sent", async () => {
+      (userOwnsAccount as jest.Mock).mockReturnValue(null);
+
+      const response = await request(app).post("/budget/transaction").send({
+        accountId: "7c5a7df3-bd02-4576-b9e5-c2c8d6cf4d21",
+        categoryId: "7c5a7df3-bd02-4576-b9e5-c2c8d6cf4d21",
         inflow: 120,
       });
       expect(response.status).toBe(200);
       expect(insertTransaction).toHaveBeenCalledTimes(1);
     });
-    // TODO: need some 503 tests? or when user id is incorrect
   });
 });
