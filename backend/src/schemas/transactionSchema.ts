536a9d6c1d199c87af421512ad3ab94f98d7985f
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2024-12-29 16:42:38 +0000

    Ensure that exactly an inflow or outflow is provided, not both

--- backend/src/schemas/transactionSchema.ts
+++ backend/src/schemas/transactionSchema.ts
@@ -1,16 +1,25 @@
 import { z } from "zod";
 
-export const transactionSchema = z.object({
-  accountId: z.string().uuid(),
-  categoryId: z.string().uuid(),
-  date: z
-    .string()
-    .refine((value) => !isNaN(new Date(value).getTime()), {
-      message: "Invalid date format",
-    })
-    .optional(),
-  inflow: z.number().optional(),
-  outflow: z.number().optional(),
-  payee: z.string().max(100).optional(),
-  memo: z.string().max(100).optional(),
-});
+export const transactionSchema = z
+  .object({
+    accountId: z.string().uuid(),
+    categoryId: z.string().uuid(),
+    date: z
+      .string()
+      .refine((value) => !isNaN(new Date(value).getTime()), {
+        message: "Invalid date format",
+      })
+      .optional(),
+    inflow: z.number().optional(),
+    outflow: z.number().optional(),
+    payee: z.string().max(100).optional(),
+    memo: z.string().max(100).optional(),
+  })
+  .refine(
+    (data) => {
+      return (data.inflow && !data.outflow) || (!data.inflow && data.outflow);
+    },
+    {
+      message: "Exactly an inflow or outflow must be provided, but not both",
+    },
+  );
