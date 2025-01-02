854e4fd7d3523c4b419e70516d4b6b274d2b637e
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-01-02 18:44:00 +0000

    Add useAddAccountMutation,   useAddTransactionMutation

--- frontend/src/core/api/budgetApiSlice.ts
+++ frontend/src/core/api/budgetApiSlice.ts
@@ -15,8 +15,31 @@ export const budgetApiSlice = apiSlice.injectEndpoints({
         url: "budget/account",
         method: "GET",
       }),
+      providesTags: ["Accounts"],
+    }),
+    // TODO: TYPING
+    addAccount: builder.mutation<any, any>({
+      query: (newAccount) => ({
+        url: "budget/account",
+        method: "POST",
+        body: newAccount,
+      }),
+      invalidatesTags: ["Accounts"],
+    }),
+    addTransaction: builder.mutation<any, any>({
+      query: (newTransaction) => ({
+        url: "budget/transaction",
+        method: "POST",
+        body: newTransaction,
+      }),
+      invalidatesTags: ["Accounts"],
     }),
   }),
 });
 
-export const { useGetDataQuery, useGetAccountsQuery } = budgetApiSlice;
+export const {
+  useGetDataQuery,
+  useGetAccountsQuery,
+  useAddAccountMutation,
+  useAddTransactionMutation,
+} = budgetApiSlice;
