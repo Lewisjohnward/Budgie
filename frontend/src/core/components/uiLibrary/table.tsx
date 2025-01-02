77d966f83e57d8927d5eb895056eb7bbde646c6e
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-01-02 18:45:10 +0000

    Remove default TableBody style

--- frontend/src/core/components/uiLibrary/table.tsx
+++ frontend/src/core/components/uiLibrary/table.tsx
@@ -28,13 +28,7 @@ const TableBody = React.forwardRef<
   HTMLTableSectionElement,
   React.HTMLAttributes<HTMLTableSectionElement>
 >(({ className, ...props }, ref) => (
-  <tbody
-    ref={ref}
-    className={cn("[&_tr:last-child]:border-0", className)}
-    {...props}
-  />
-))
-TableBody.displayName = "TableBody"
+  <tbody ref={ref} className={cn(className)} {...props} />
 
 const TableFooter = React.forwardRef<
   HTMLTableSectionElement,
