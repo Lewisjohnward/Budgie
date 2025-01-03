8a8c948042434bc6594524a4d75f85a9551484c1
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-01-03 14:04:54 +0000

    Add transaction category name to normalized data

--- backend/src/utility/normalizeData.ts
+++ backend/src/utility/normalizeData.ts
@@ -23,11 +23,13 @@ type Transaction = {
   cleared: boolean;
   createdAt: Date;
   updatedAt: Date;
+  category: Category;
 };
 
 type Category = {
   id: string;
-  type: "EXPENSE" | "INCOME";
+  userId: string;
+  // type: "EXPENSE" | "INCOME";
   name: string;
 };
 
@@ -45,7 +47,6 @@ export function normalizeData(data: { accounts: Account[] }): NormalizedData {
   };
 
   data.accounts.forEach((account) => {
-    // Normalize accounts
     normalizedData.accounts[account.id] = {
       id: account.id,
       userId: account.userId,
@@ -54,34 +55,20 @@ export function normalizeData(data: { accounts: Account[] }): NormalizedData {
       balance: account.balance,
       createdAt: account.createdAt,
       updatedAt: account.updatedAt,
-      // Store transaction IDs for later reference
       transactions: [],
     };
 
     account.transactions.forEach((transaction) => {
-      // Normalize transactions
       normalizedData.transactions[transaction.id] = {
-        id: transaction.id,
-        accountId: transaction.accountId,
-        categoryId: transaction.categoryId,
-        date: transaction.date,
-        inflow: transaction.inflow,
-        outflow: transaction.outflow,
-        payee: transaction.payee,
-        memo: transaction.memo,
-        cleared: transaction.cleared,
-        createdAt: transaction.createdAt,
-        updatedAt: transaction.updatedAt,
+        ...transaction,
       };
 
-      // Normalize categories (if not already added)
       if (!normalizedData.categories[transaction.categoryId]) {
         normalizedData.categories[transaction.categoryId] = {
           id: transaction.categoryId,
-          type: "EXPENSE", // Assuming `type` exists within transaction
-          name: "placeholder", // Assuming `name` exists within transaction
-          // type: transaction.type, // Assuming `type` exists within transaction
-          // name: transaction.name, // Assuming `name` exists within transaction
+          userId: transaction.category.userId,
+          // type: "EXPENSE",
+          name: transaction.category.name,
         };
       }
     });
