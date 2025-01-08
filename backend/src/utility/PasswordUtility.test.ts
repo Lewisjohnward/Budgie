ef406443ace9f701e5a7fbd7ccb5d7b3c4fb3f5d
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-01-08 14:32:07 +0000

    Update test styles

--- backend/src/utility/PasswordUtility.test.ts
+++ backend/src/utility/PasswordUtility.test.ts
@@ -4,66 +4,64 @@ import {
   DecodeToken,
   GenerateAccessToken,
   GenerateRefreshToken,
+  ValidateSignature,
 } from "./PasswordUtility";
 
 describe("Password utility", () => {
-  it("Signing/Decoding access token works", () => {
-    const claims = { _id: "id", email: "test@email.com" };
-    const accessToken = GenerateAccessToken(claims);
-    const decoded = DecodeToken(accessToken);
-    expect(decoded).toEqual({
-      ...claims,
-      iat: expect.any(Number),
-      exp: expect.any(Number),
+  describe("Generate refresh/access token", () => {
+    it("Signing/Decoding access token works", () => {
+      const claims = { _id: "id", email: "test@email.com" };
+      const accessToken = GenerateAccessToken(claims);
+      const decoded = DecodeToken(accessToken);
+      expect(decoded).toEqual({
+        ...claims,
+        iat: expect.any(Number),
+        exp: expect.any(Number),
+      });
+    });
+
+    it("Signing/Decoding access token with wrong key throws error", () => {
+      const claims = { _id: "id", email: "test@email.com" };
+      const accessToken = GenerateAccessToken(claims);
+
+      try {
+        jwt.verify(accessToken, "WRONG_KEY");
+      } catch (error) {
+        expect(error instanceof jwt.JsonWebTokenError).toBeTruthy();
+      }
+    });
+
+    it("Signing/Decoding refresh token works", () => {
+      const claims = { _id: "id", email: "test@email.com" };
+      const accessToken = GenerateRefreshToken(claims);
+      const decoded = DecodeToken(accessToken);
+      expect(decoded).toEqual({
+        ...claims,
+        iat: expect.any(Number),
+        exp: expect.any(Number),
+      });
+    });
+
+    it("Signing/Decoding refresh token with wrong key throws error", () => {
+      const claims = { _id: "id", email: "test@email.com" };
+      const accessToken = GenerateRefreshToken(claims);
+
+      try {
+        jwt.verify(accessToken, "WRONG_KEY");
+      } catch (error) {
+        expect(error instanceof jwt.JsonWebTokenError).toBeTruthy();
+      }
     });
   });
+  describe("ValidateSignature", () => {
+    it.todo("should return true and populate req.user for a valid token");
 
-  it("Signing/Decoding access token with wrong key throws error", () => {
-    const claims = { _id: "id", email: "test@email.com" };
-    const accessToken = GenerateAccessToken(claims);
+    it.todo("Should return false when signature is undefined");
 
-    try {
-      jwt.verify(accessToken, "WRONG_KEY");
-    } catch (error) {
-      expect(error instanceof jwt.JsonWebTokenError).toBeTruthy();
-    }
-    //
-    // const accessToken = GenerateAccessToken(claims);
-    // const decoded = DecodeToken(accessToken);
-    // expect(decoded).toEqual({
-    //   ...claims,
-    //   iat: expect.any(Number),
-    //   exp: expect.any(Number),
-    // });
+    it.todo("Should append req.user if decoded correctly");
+
+    it.todo("Should return true if decoded correctly");
+
+    it.todo("Should return false if decode fails");
   });
-
-  it("Signing/Decoding refresh token works", () => {
-    const claims = { _id: "id", email: "test@email.com" };
-    const accessToken = GenerateRefreshToken(claims);
-    const decoded = DecodeToken(accessToken);
-    expect(decoded).toEqual({
-      ...claims,
-      iat: expect.any(Number),
-      exp: expect.any(Number),
-    });
-  });
-
-  it("Signing/Decoding refresh token with wrong key throws error", () => {
-    const claims = { _id: "id", email: "test@email.com" };
-    const accessToken = GenerateRefreshToken(claims);
-
-    try {
-      jwt.verify(accessToken, "WRONG_KEY");
-    } catch (error) {
-      expect(error instanceof jwt.JsonWebTokenError).toBeTruthy();
-    }
-  });
-
-  // it("Not found for site 404", async () => {});
-
-  // it("Health check route returns valid response", async () => {});
 });
-
-// afterAll(async () => {
-//   await db.end()
-// })
