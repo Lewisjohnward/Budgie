9b773f0b65af5d24c2f749a619aba0fc7a539864
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-01-09 13:25:20 +0000

    Context menu with delete/duplicate/move

--- frontend/src/pages/budget/account/components/Table.tsx
+++ frontend/src/pages/budget/account/components/Table.tsx
@@ -7,7 +7,7 @@ import {
   TableRow,
 } from "@/core/components/uiLibrary/table";
 import { ReactNode, useState } from "react";
-import { ArrowDown, ArrowUp } from "lucide-react";
+import { ArrowDown, ArrowUp, Copy, MoveRight, Trash2 } from "lucide-react";
 import { Button } from "@/core/components/uiLibrary/button";
 import { Checkbox } from "@/core/components/uiLibrary/checkbox";
 import {
@@ -21,6 +21,13 @@ import {
 } from "@tanstack/react-table";
 import { Transaction } from "../Account";
 import clsx from "clsx";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/core/components/uiLibrary/context-menu";
+import { useDeleteTransactionMutation } from "@/core/api/budgetApiSlice";
 
 type TableProps = {
   transactions: Transaction[];
@@ -170,11 +177,39 @@ function SortButton({
   );
 }
 
+const useTransactionManager = () => {
+  const [deleteTransaction] = useDeleteTransactionMutation();
+
+  const handleDeleteTranscation = (id: string) => {
+    deleteTransaction({ transactionIds: [id] });
+  };
+
+  const handleDuplicateTransaction = (id: string) => {
+    console.log("dup");
+  };
+
+  const handleMoveTransaction = (id: string) => {
+    console.log("move");
+  };
+
+  return {
+    handleDeleteTranscation,
+    handleDuplicateTransaction,
+    handleMoveTransaction,
+  };
+};
+
 export function MyTable({ transactions }: TableProps) {
   const [sorting, setSorting] = useState<SortingState>([]);
   const [highlightedRow, setHighlightedRow] = useState<number | null>(null);
   const [rowSelection, setRowSelection] = useState({});
   const [editingRow, setEditingRow] = useState<number | null>(null);
+  const [disableContextMenu, setDisableContextMenu] = useState<boolean>(false);
+  const {
+    handleDeleteTranscation,
+    handleDuplicateTransaction,
+    handleMoveTransaction,
+  } = useTransactionManager();
 
   const table = useReactTable({
     data: transactions,
@@ -199,98 +234,147 @@ export function MyTable({ transactions }: TableProps) {
   ) => {
     console.log(e.shiftKey);
     if (highlightedRow === rowIndex) {
+      setDisableContextMenu(true);
       setEditingRow(rowIndex);
     } else {
+      setDisableContextMenu(false);
       setHighlightedRow(rowIndex);
       setEditingRow(null);
     }
   };
 
   return (
-    <div className="overflow-x-scroll">
-      <Table
-        className="table-fixed select-none"
-        style={{ width: `${table.getTotalSize()}px` }}
-      >
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                const isResizable = header.column.getCanResize();
-                return (
-                  <TableHead
-                    key={header.id}
-                    className="relative border border-r-neutral-300 text-nowrap text-ellipsis overflow-hidden"
-                    style={{ width: `${header.getSize()}px` }}
-                  >
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )}
-                    <div
-                      onMouseDown={header.getResizeHandler()}
-                      onTouchStart={header.getResizeHandler()}
-                      className={clsx(
-                        isResizable
-                          ? "cursor-col-resize hover:bg-blue-800"
-                          : "cursor-auto",
-                        "absolute top-0 right-0 w-[2px] h-full",
-                      )}
-                    />
-                    {/*   style={{ */}
-                    {/*   border: '1px solid black', */}
-                    {/*   padding: '10px', */}
-                    {/*   textAlign: 'left', */}
-                    {/*   background: '#f4f4f4', */}
-                    {/*   cursor: column.getIsResizing() ? 'col-resize' : 'pointer', */}
-                    {/*   width: column.getSize(), */}
-                    {/*   maxWidth: column.columnDef.maxWidth, // Max width applied here */}
-                    {/* }} */}
-                  </TableHead>
-                );
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody className="border border-r-neutral-300">
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <>
-                <TableRow
-                  key={row.id}
-                  data-state={highlightedRow === row.index && "selected"}
-                  className="data-[state=selected]:bg-sky-900/10 cursor-pointer"
-                  onClick={(e) => handleRowClick(e, row.index)}
+    <Table
+      className="table-fixed select-none"
+      style={{ width: `${table.getTotalSize()}px` }}
+      onContextMenu={(e) => {
+        e.preventDefault();
+        setEditingRow(null);
+        setDisableContextMenu(false);
+      }}
+    >
+      <TableHeader>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              const isResizable = header.column.getCanResize();
+              return (
+                <TableHead
+                  key={header.id}
+                  className="relative border border-r-neutral-300 text-nowrap text-ellipsis overflow-hidden"
+                  style={{ width: `${header.getSize()}px` }}
                 >
-                  {row.getVisibleCells().map((cell) => (
-                    <TableCell
-                      key={cell.id}
-                      style={{ width: `${cell.column.getSize()}px` }}
-                    >
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext(),
-                      )}
-                    </TableCell>
-                  ))}
-                </TableRow>
-
-                {row.getIsSelected() && editingRow === row.index && (
-                  <div>hello</div>
-                )}
-              </>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+                  <div
+                    onMouseDown={header.getResizeHandler()}
+                    onTouchStart={header.getResizeHandler()}
+                    className={clsx(
+                      isResizable
+                        ? "cursor-col-resize hover:bg-blue-950"
+                        : "cursor-auto",
+                      "absolute top-0 right-0 w-[2px] h-full",
+                    )}
+                  />
+                </TableHead>
+              );
+            })}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody className="border border-r-neutral-300">
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row) => (
+            <>
+              <ContextMenu>
+                <ContextMenuTrigger asChild disabled={disableContextMenu}>
+                  <TableRow
+                    key={row.id}
+                    data-state={highlightedRow === row.index && "selected"}
+                    className={clsx(
+                      editingRow === row.index && "border-b-0",
+                      "data-[state=selected]:bg-sky-950/20 cursor-pointer",
+                    )}
+                    onClick={(e) => {
+                      handleRowClick(e, row.index);
+                    }}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell
+                        key={cell.id}
+                        style={{ width: `${cell.column.getSize()}px` }}
+                      >
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext(),
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                </ContextMenuTrigger>
+                <ContextMenuContent className="w-40">
+                  <ContextMenuItem
+                    onClick={() => handleDeleteTranscation(row.original.id)}
+                  >
+                    Delete
+                    <div>
+                      <Trash2 size={15} />
+                    </div>
+                  </ContextMenuItem>
+                  <ContextMenuItem
+                    onClick={() => handleDuplicateTransaction(row.original.id)}
+                  >
+                    Duplicate
+                    <div>
+                      <Copy size={15} />
+                    </div>
+                  </ContextMenuItem>
+                  <ContextMenuItem
+                    disabled
+                    onClick={() => handleMoveTransaction(row.original.id)}
+                  >
+                    Move to account
+                    <div>
+                      <MoveRight size={15} />
+                    </div>
+                  </ContextMenuItem>
+                </ContextMenuContent>
+              </ContextMenu>
+              {highlightedRow === row.index && editingRow === row.index && (
+                <TableCell
+                  colSpan={7}
+                  className="bg-sky-950/20"
+                  onClick={() => setEditingRow(null)}
+                >
+                  <div className="flex justify-end">
+                    <div className="space-x-2">
+                      <Button
+                        variant={"outline"}
+                        className="text-sky-950/80 bg-transparent border-blue-950/80 hover:bg-sky-950/30"
+                      >
+                        Cancel
+                      </Button>
+                      <Button className="text-white bg-sky-950/80 hover:bg-sky-950/30">
+                        Save
+                      </Button>
+                    </div>
+                  </div>
+                </TableCell>
+              )}
+            </>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell colSpan={columns.length} className="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
   );
 }
