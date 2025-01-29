c9d4ed19879796f7e44945f0bc912c4d0e344dd2
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-01-29 10:56:51 +0000

    squash: expand/close subcategories

--- frontend/src/pages/budget/allocation/components/categories/Categories.tsx
+++ frontend/src/pages/budget/allocation/components/categories/Categories.tsx
@@ -3,64 +3,50 @@ import { AddCircleIcon, ChevronDownIcon } from "@/core/icons/icons";
 import { bgGray, borderBottom, darkBlueText } from "@/core/theme/colors";
 import clsx from "clsx";
 
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 
-const categories: Category[] = [
-  {
-    name: "Bills",
-    open: true,
-    subCategories: [
-      {
-        name: "Rent",
-        assigned: 0,
-        activity: 0,
-        available: 0,
-      },
-      {
+const data = {
+  entities: {
+    categories: {
+      1: { id: 1, name: "Bills", open: true, subCategories: [1, 2] },
+      2: { id: 2, name: "Needs", open: true, subCategories: [3, 4] },
+      3: { id: 3, name: "Wants", open: true, subCategories: [5, 6] },
+    },
+    subCategories: {
+      1: { id: 1, name: "Rent", assigned: 200, activity: 50, available: 20 },
+      2: {
+        id: 2,
         name: "Utilities",
-        assigned: 0,
-        activity: 0,
-        available: 0,
+        assigned: 100,
+        activity: 30,
+        available: 10,
       },
-    ],
-  },
-  {
-    name: "Needs",
-    open: true,
-    subCategories: [
-      {
+      3: {
+        id: 3,
         name: "Groceries",
-        assigned: 0,
-        activity: 0,
-        available: 0,
+        assigned: 300,
+        activity: 80,
+        available: 50,
       },
-      {
+      4: {
+        id: 4,
         name: "Retirement",
-        assigned: 0,
-        activity: 0,
-        available: 0,
+        assigned: 150,
+        activity: 60,
+        available: 40,
       },
-    ],
-  },
-  {
-    name: "Wants",
-    open: true,
-    subCategories: [
-      {
-        name: "Hobbies",
-        assigned: 0,
-        activity: 0,
-        available: 0,
-      },
-      {
+      5: { id: 5, name: "Hobbies", assigned: 50, activity: 20, available: 10 },
+      6: {
+        id: 6,
         name: "Budgie subscription",
-        assigned: 0,
-        activity: 0,
-        available: 0,
+        assigned: 75,
+        activity: 25,
+        available: 15,
       },
-    ],
+    },
   },
-];
+  result: [1, 2, 3],
+};
 
 type Category = {
   name: string;
@@ -76,28 +62,55 @@ type SubCategory = {
 };
 
 export default function Categories() {
+  const [displayAllSubCategories, setDisplayAllSubCategories] = useState(false);
+
+  const categories = Object.values(data.entities.categories);
+  const subCategories = Object.values(data.entities.subCategories);
+
+  console.log("categories", categories);
+  console.log("subcategories", subCategories);
+
+  const handleExpand = () => {
+    console.log("Hello, World!");
+  };
+
   return (
-    <>
+    <div>
       <div className="flex items-center gap-2 py-2 px-2">
         <button>
-          <ChevronDownIcon className={`h-4 w-4 ${darkBlueText}`} />
+          <ChevronDownIcon
+            className={clsx(
+              displayAllSubCategories ? "" : "-rotate-90",
+              `h-4 w-4 ${darkBlueText}`,
+            )}
+            onClick={() => setDisplayAllSubCategories(!displayAllSubCategories)}
+          />
         </button>
         <div className={`${darkBlueText} font-thin`}>CATEGORY</div>
       </div>
 
       {categories.map((category) => (
-        <Category category={category} />
+        <Category name={category.name}>
+          {/* category header ? */}
+          {category.subCategories.map((subCategory) => (
+            <SubCategory subCategory={subCategories[subCategory - 1]} />
+          ))}
+        </Category>
       ))}
-    </>
+    </div>
   );
 }
 
-function Category({ category }: { category: Category }) {
-  const { name, open, subCategories } = category;
+function Category({ children, name }: { children: ReactNode; name: string }) {
+  const [displaySubCategories, setDisplayCategories] = useState(false);
   const [hover, setHover] = useState(false);
 
   const handleMouseEnter = () => setHover(true);
   const handleMouseExit = () => setHover(false);
+
+  const handleToggleDisplaySubCategory = () => {
+    setDisplayCategories(!displaySubCategories);
+  };
 
   return (
     <div className="min-w-[600px]">
@@ -106,10 +119,10 @@ function Category({ category }: { category: Category }) {
         onMouseLeave={handleMouseExit}
         className={`flex items-center gap-2 px-2 ${bgGray}`}
       >
-        <button className="w-4">
+        <button className="w-4" onClick={handleToggleDisplaySubCategory}>
           <ChevronDownIcon
             className={clsx(
-              open ? "" : "-rotate-90",
+              displaySubCategories ? "rotate-0" : "-rotate-90",
               `h-4 w-4 ${darkBlueText}`,
             )}
           />
@@ -135,10 +148,7 @@ function Category({ category }: { category: Category }) {
           <p>Available</p>
         </div>
       </div>
-
-      {subCategories.map((subCategory) => (
-        <SubCategory subCategory={subCategory} />
-      ))}
+      {displaySubCategories && children}
     </div>
   );
 }
