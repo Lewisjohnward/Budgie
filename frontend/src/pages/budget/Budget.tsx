6d049b067c2f64762662d015042e00a820af530a
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-02-12 11:26:04 +0000

    Add add category group popover

--- frontend/src/pages/budget/Budget.tsx
+++ frontend/src/pages/budget/Budget.tsx
@@ -18,15 +18,40 @@ function BudgetContent() {
 }
 
 export function Menu() {
+
+  const handleClick = async () => {
+  };
+
   return (
     <div className="px-2">
-      <button
-        className="flex items-center gap-2 px-2 py-2 text-sky-950 rounded text-sm hover:bg-sky-950/10"
-        onClick={() => {}}
-      >
-        <CirclePlus size={15} />
-        Category Group
-      </button>
+      <Popover modal={true}>
+        <PopoverTrigger>
+          <button
+            className="flex items-center gap-2 px-2 py-2 text-sky-950 rounded text-sm hover:bg-sky-950/10"
+            onClick={handleClick}
+          >
+            <CirclePlus size={15} />
+            Category Group
+          </button>
+        </PopoverTrigger>
+        <PopoverPortal>
+          <PopoverContent side={"bottom"} className="w-[200px] p-0 shadow-lg">
+            <PopoverArrow className="w-8 h-2 fill-white" />
+            <div className="px-2 py-2 space-y-2">
+              <Input
+                className="shadow-none focus-visible:ring-sky-950"
+                placeholder="New Category Group"
+              />
+              <div className="flex justify-end gap-2">
+                <Button className="bg-gray-400/80 hover:bg-gray-400/60">
+                  Cancel
+                </Button>
+                <Button className="bg-sky-900 hover:bg-sky-950/80">Okay</Button>
+              </div>
+            </div>
+          </PopoverContent>
+        </PopoverPortal>
+      </Popover>
     </div>
   );
 }
