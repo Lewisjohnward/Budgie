e3b3fe509d6a05531692d455cefec0e01d07f6e8
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-02-15 14:24:51 +0000

    Change schema to match FE

--- backend/src/schemas/registerUserSchema.ts
+++ backend/src/schemas/registerUserSchema.ts
@@ -1,19 +1,16 @@
 import { z } from "zod";
 
 export const registerUserSchema = z.object({
-  email: z.string().email({ message: "Invalid email address" }),
+  email: z.string().email("Please enter a valid email address"),
   password: z
     .string()
-    .min(8, { message: "Password must be at least 8 characters long" })
-    .max(64, { message: "Password must be no more than 64 characters long" })
-    .regex(/[A-Z]/, {
-      message: "Password must contain at least one uppercase letter",
-    })
-    .regex(/[a-z]/, {
-      message: "Password must contain at least one lowercase letter",
-    })
-    .regex(/[0-9]/, { message: "Password must contain at least one number" })
-    .regex(/[\W_]/, {
-      message: "Password must contain at least one special character",
-    }),
+    .min(
+      12,
+      "Password must be at least 12 characters long and contain at least one number or symbol",
+    )
+    .max(64, "Password must be no more than 64 characters long")
+    .regex(
+      /(?=.*[0-9!@#$%^&*()_+{}[\]:;<>,.?/~`-])/,
+      "Password must contain at least one number or symbol",
+    ),
 });
