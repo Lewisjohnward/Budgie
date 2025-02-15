6e9ae54724e76c25f436385e68129da4cf50ce38
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-02-15 13:57:50 +0000

    Extract signup schema

---
+++ frontend/src/core/schemas/signupSchema.ts
@@ -0,0 +1,26 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
+  password: z
+    .string()
+    .min(
+      12,
+      "Password must be at least 12 characters long and contain at least one number or symbol",
+    )
+    .regex(
+      /(?=.*[0-9!@#$%^&*()_+{}[\]:;<>,.?/~`-])/,
+      "Password must contain at least one number or symbol",
+    ),
+});
+
+const customErrorMap: z.ZodErrorMap = (issue, ctx) => {
+  if (issue.code === z.ZodIssueCode.invalid_type) {
+    return { message: "This field is required" };
+  }
+  return { message: ctx.defaultError };
+};
+
+z.setErrorMap(customErrorMap);
+
+export type SignUpType = z.infer<typeof signupSchema>;
