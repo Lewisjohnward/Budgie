c70593c6b13fb0cb994323667ab76d4233f32a14
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-02-18 11:49:07 +0000

    Set max length on sign up password

--- frontend/src/core/schemas/signupSchema.ts
+++ frontend/src/core/schemas/signupSchema.ts
@@ -8,6 +8,7 @@ export const signupSchema = z.object({
       12,
       "Password must be at least 12 characters long and contain at least one number or symbol",
     )
+    .max(64, "Password must be no more than 64 characters long")
     .regex(
       /(?=.*[0-9!@#$%^&*()_+{}[\]:;<>,.?/~`-])/,
       "Password must contain at least one number or symbol",
