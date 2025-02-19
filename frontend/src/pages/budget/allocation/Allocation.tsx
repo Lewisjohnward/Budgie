6703bceaa00170190438d6e7471131a2d9134247
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-02-19 13:38:11 +0000

    Create custom hook for displaying allocation

--- frontend/src/pages/budget/allocation/Allocation.tsx
+++ frontend/src/pages/budget/allocation/Allocation.tsx
@@ -1,17 +1,79 @@
-import Header from "./components/header/Header";
-import Body from "./components/categories/Categories";
-import { ReactNode } from "react";
-import { bgGray, borderBottom } from "@/core/theme/colors";
+import { ReactNode, useEffect, useState } from "react";
 import Assign from "./components/assign/Assign";
 import { Menu } from "../Budget";
 import {
   useGetAccountsQuery,
   useGetCategoriesQuery,
 } from "@/core/api/budgetApiSlice";
+import { normalizedBudgetData } from "./mockData";
+function formatDate(isoString: string) {
+  return new Date(isoString).toLocaleDateString("en-US", {
+    year: "2-digit",
+    month: "short",
+  });
+}
+
+function useAllocation() {
+  useEffect(() => {}, []);
+
+  const [allocationData, setAllocationData] = useState(
+    produce(normalizedBudgetData, (draft) => {
+      Object.values(draft.months).forEach((month) => {
+        month.current = false;
+      });
+    }),
+  );
+
+  const [selector, setSelector] = useState(0);
+  const maxSelector = Object.keys(normalizedBudgetData).length - 1;
+  const minSelector = 0;
+
+  const { categoryGroups, categories } = allocationData;
+  const months = Object.values(normalizedBudgetData.months).map((month, i) => ({
+    ...month,
+    current: i === selector ? true : false,
+    formattedName: formatDate(month.month),
+  }));
+
+  const currentMonth = months.find((month) => month.current);
+
+  const derivedCategoryGroups = currentMonth.categoryGroupIds.map(
+    (catGroupId) => categoryGroups[catGroupId],
+  );
+
+  const nextMonth = () =>
+    setSelector((prev) => (prev + 1 < maxSelector ? prev + 1 : prev));
+  const prevMonth = () =>
+    setSelector((prev) => (prev - 1 >= minSelector ? prev - 1 : prev));
+
+  const categoriesSelector = [
+    "All",
+    "Underfunded",
+    "Money available",
+    "Snoozed",
+  ];
+
+  return {
+    currentMonth,
+    categoriesSelector,
+    categoryGroups: derivedCategoryGroups,
+    categories,
+    nextMonth,
+    prevMonth,
+  };
+}
 
 export function Allocation() {
   const { isLoading: isLoadingAccounts } = useGetAccountsQuery();
   const { isLoading: isLoadingCategories } = useGetCategoriesQuery();
+  const {
+    categoriesSelector,
+    currentMonth,
+    categoryGroups,
+    categories,
+    nextMonth,
+    prevMonth,
+  } = useAllocation();
 
   if (isLoadingCategories || isLoadingAccounts) return "...Loading";
 
