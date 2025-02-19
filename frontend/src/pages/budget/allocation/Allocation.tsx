bfa81f7a00efc42f1af828a9e6dc9cc340867a5c
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-02-19 14:27:47 +0000

    Move Categories into Allocation

--- frontend/src/pages/budget/allocation/Allocation.tsx
+++ frontend/src/pages/budget/allocation/Allocation.tsx
@@ -1,5 +1,23 @@
 import { AssignedMoney, MonthSelector } from "./components/header/Header";
-import { ReactNode, useEffect, useState } from "react";
+import { AddCircleIcon, ChevronDownIcon } from "@/core/icons/icons";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/core/components/uiLibrary/popover";
+import {
+  forwardRef,
+  ReactNode,
+  RefObject,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from "react";
+import { PopoverArrow, PopoverPortal } from "@radix-ui/react-popover";
+import { Input } from "@/core/components/uiLibrary/input";
+import { Button } from "@/core/components/uiLibrary/button";
+import { z } from "zod";
 import {
   bgGray,
   borderBottom,
@@ -12,12 +30,14 @@ import {
   useGetAccountsQuery,
   useGetCategoriesQuery,
 } from "@/core/api/budgetApiSlice";
-import Categories from "./components/categories/Categories";
 import { normalizedBudgetData } from "./mockData";
 import { produce } from "immer";
 import useMouseOverTimeout from "@/core/hooks/useMouseOverTimeout";
-import { AddCircleIcon } from "@/core/icons/icons";
 import clsx from "clsx";
+import { useForm } from "react-hook-form";
+import { Checkbox } from "@radix-ui/react-checkbox";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Progress } from "@/core/components/uiLibrary/progress";
 
 function formatDate(isoString: string) {
   return new Date(isoString).toLocaleDateString("en-US", {
@@ -38,6 +58,7 @@ function useAllocation() {
   );
 
   const [selector, setSelector] = useState(0);
+  const [state, setState] = useState();
   const maxSelector = Object.keys(normalizedBudgetData).length - 1;
   const minSelector = 0;
 
@@ -54,11 +75,6 @@ function useAllocation() {
     (catGroupId) => categoryGroups[catGroupId],
   );
 
-  const nextMonth = () =>
-    setSelector((prev) => (prev + 1 < maxSelector ? prev + 1 : prev));
-  const prevMonth = () =>
-    setSelector((prev) => (prev - 1 >= minSelector ? prev - 1 : prev));
-
   const categoriesSelector = [
     "All",
     "Underfunded",
@@ -66,7 +82,18 @@ function useAllocation() {
     "Snoozed",
   ];
 
+  const nextMonth = () =>
+    setSelector((prev) => (prev + 1 < maxSelector ? prev + 1 : prev));
+  const prevMonth = () =>
+    setSelector((prev) => (prev - 1 >= minSelector ? prev - 1 : prev));
+
+  const handleAddCategory = (data: AddCategoryFormData) => {
+    // addCategory(data);
+    // appendedCategoryGroupId.current = data.categoryGroupId;
+  };
+
   return {
+    handleAddCategory,
     currentMonth,
     categoriesSelector,
     categoryGroups: derivedCategoryGroups,
@@ -88,36 +115,12 @@ export function Allocation() {
     categories,
     nextMonth,
     prevMonth,
+    handleAddCategory,
   } = useAllocation();
 
   if (isLoadingCategories || isLoadingAccounts) return "...Loading";
   return (
     <AllocationContainer>
-      {/* <div className="flex gap-2"> */}
-      {/*   <button onClick={prevMonth}>p</button> */}
-      {/*   <div className="text-xl font-bold">{currentMonth.formatted}</div> */}
-      {/*   <button onClick={nextMonth}>n</button> */}
-      {/* </div> */}
-      {/* {categoryGroups.map((categoryGroup) => { */}
-      {/*   // @ts-ignore */}
-      {/*   const { name, categoryIds } = categoryGroup; */}
-      {/*   // @ts-ignore */}
-      {/*   return ( */}
-      {/*     <div> */}
-      {/*       <div>{name}</div> */}
-      {/*       <div> */}
-      {/*         {categoryGroup.categoryIds.map((categoryId) => { */}
-      {/*           return ( */}
-      {/*             <div> */}
-      {/*               <p>{categories[categoryId].name}</p> */}
-      {/*               <p>{categories[categoryId].amounts[currentMonth.month]}</p> */}
-      {/*             </div> */}
-      {/*           ); */}
-      {/*         })} */}
-      {/*       </div> */}
-      {/*     </div> */}
-      {/*   ); */}
-      {/* })} */}
       <HeaderContainer>
         <div className="flex gap-8 py-4">
           <MonthSelector
@@ -131,15 +134,92 @@ export function Allocation() {
           {categoriesSelector.map((catSelector) => (
             <Category key={catSelector} text={catSelector} />
           ))}
-          <AddCategoryButton />
+          <AddCategorySelectorButton />
         </CategorySelectorContainer>
       </HeaderContainer>
 
       <BodyContainer>
-        <CategoriesContainer>
+        <WideCategoriesContainer>
           <Menu />
-          <Categories />
-        </CategoriesContainer>
+          <div className="flex items-center gap-2 p-2">
+            {categoryGroups.length > 0 ? (
+              <button>
+                <ChevronDownIcon
+                  className={clsx(
+                    // atLeastOneCategoryGroupExpanded ? "" : "-rotate-90",
+                    false ? "" : "-rotate-90",
+                    `h-4 w-4 ${darkBlueText}`,
+                  )}
+                  // onClick={() => toggleDisplayCategories(undefined)}
+                  onClick={() => {}}
+                />
+              </button>
+            ) : null}
+            <div className={`${darkBlueText} font-thin`}>CATEGORY</div>
+          </div>
+          {categoryGroups.map((group) => {
+            const { categoryIds } = group;
+            console.log("hello", categoryIds);
+            return (
+              <Container key={group.id}>
+                <CategoryGroupContainer>
+                  <ExpandCategoryGroup
+                    // onClick={() => toggleDisplayCategories(group.id)}
+                    onClick={() => {}}
+                    // open={group.open}
+                    open={true}
+                  />
+                  <Checkbox className="size-3 rounded-[2px] shadow-none" />
+                  <CategoryGroupName>{group.name}</CategoryGroupName>
+                  <AddCategoryButton
+                    id={group.id}
+                    handleAddCategory={handleAddCategory}
+                  />
+                </CategoryGroupContainer>
+
+                <CategoriesContainer
+                  display={true}
+                  // display={group.open}
+                >
+                  {categoryIds.length > 0
+                    ? categoryIds.map((categoryId) => {
+                        const category = categories[categoryId];
+                        const { id, name } = category;
+                        const activity = category.amounts[currentMonth.month];
+
+                        // const available = assigned - activity;
+
+                        return (
+                          <CategoryContent key={id}>
+                            {(ref) => (
+                              <>
+                                <Checkbox className="size-3 rounded-[2px] shadow-none" />
+                                <CategoryNameContainer>
+                                  <CategoryName>{name}</CategoryName>
+                                  <ProgressBar
+                                    assigned={0}
+                                    activity={activity}
+                                    available={0}
+                                  />
+                                </CategoryNameContainer>
+                                <EditAssigned
+                                  ref={ref}
+                                  // assigned={0.toFixed(2)}
+                                  assigned={"0.00"}
+                                />
+                                <Activity>{activity.toFixed(2)}</Activity>
+                                <Available>{"0.00"}</Available>
+                              </>
+                            )}
+                          </CategoryContent>
+                        );
+                      })
+                    : null}
+                </CategoriesContainer>
+              </Container>
+            );
+          })}
+        </WideCategoriesContainer>
         <AssignContainer>
           <Assign />
         </AssignContainer>
@@ -156,7 +236,7 @@ function BodyContainer({ children }: { children: ReactNode }) {
   return <div className="flex">{children}</div>;
 }
 
-function CategoriesContainer({ children }: { children: ReactNode }) {
+function WideCategoriesContainer({ children }: { children: ReactNode }) {
   return <div className="flex-grow-2"> {children}</div>;
 }
 
@@ -184,7 +264,7 @@ function Category({ text }: { text: string }) {
   );
 }
 
-function AddCategoryButton() {
+function AddCategorySelectorButton() {
   const { mouseOver, handleMouseOver } = useMouseOverTimeout();
 
   return (
@@ -197,4 +277,294 @@ function AddCategoryButton() {
       />
     </button>
   );
+}
+
+/////// CATEGORIES ALLOCATION
+
+function Container({ children }: { children: ReactNode }) {
+  return <div className="min-w-[600px]">{children}</div>;
+}
+
+function CategoryGroupContainer({ children }: { children: ReactNode }) {
+  return (
+    <div className={`group flex items-center gap-0 pl-2 bg-gray-400/20`}>
+      <div className="flex-grow flex-shrink basis-56 flex items-center gap-4 py-2">
+        {children}
+      </div>
+      <div className="flex-1 text-right">
+        <p>Assigned</p>
+      </div>
+      <div className="flex-1 text-right">
+        <p>Activity</p>
+      </div>
+      <div className="flex-1 text-right">
+        <p>Available</p>
+      </div>
+    </div>
+  );
+}
+
+function CategoryGroupName({ children }: { children: ReactNode }) {
+  return <p className={`${darkBlueText} font-bold`}>{children}</p>;
+}
+
+const AddCategorySchema = z.object({
+  categoryGroupId: z.string(),
+  name: z.string().min(1),
+});
+
+type AddCategoryFormData = z.infer<typeof AddCategorySchema>;
+
+function AddCategoryButton({
+  id,
+  handleAddCategory,
+}: {
+  id: string;
+  handleAddCategory: (data: AddCategoryFormData) => void;
+}) {
+  const [displayAddCategory, setDisplayAddCategory] = useState(false);
+  const close = () => {
+    setDisplayAddCategory(false);
+    reset();
+  };
+  const open = () => setDisplayAddCategory(true);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { isValid },
+    reset,
+  } = useForm<AddCategoryFormData>({
+    defaultValues: {
+      categoryGroupId: id,
+    },
+    resolver: zodResolver(AddCategorySchema),
+  });
+
+  const createCategory = (data: AddCategoryFormData) => {
+    handleAddCategory(data);
+    close();
+    reset();
+  };
+
+  return (
+    <Popover open={displayAddCategory} modal={true}>
+      <PopoverTrigger onClick={open}>
+        <AddCircleIcon
+          className={`${darkBlueText} invisible group-hover:visible`}
+        />
+      </PopoverTrigger>
+      <PopoverPortal>
+        <PopoverContent
+          onPointerDownOutside={close}
+          avoidCollisions={false}
+          side={"right"}
+          className="w-[200px] p-0 shadow-lg"
+        >
+          <PopoverArrow className="w-8 h-2 fill-white" />
+          <form
+            className="px-2 py-2 space-y-2"
+            onSubmit={handleSubmit(createCategory)}
+          >
+            <Input
+              className="shadow-none focus-visible:ring-sky-950"
+              placeholder="New Category"
+              autoComplete="off"
+              {...register("name")}
+            />
+            <div className="flex justify-end gap-2">
+              <Button
+                type="reset"
+                onClick={close}
+                className="bg-gray-400 hover:bg-gray-400/80"
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                className="bg-sky-900 hover:bg-sky-950/80"
+                disabled={!isValid}
+              >
+                Okay
+              </Button>
+            </div>
+          </form>
+        </PopoverContent>
+      </PopoverPortal>
+    </Popover>
+  );
+}
+
+function ExpandCategoryGroup({
+  onClick,
+  open,
+}: {
+  onClick: () => void;
+  open: boolean;
+}) {
+  return (
+    <button className="w-4" onClick={onClick}>
+      <ChevronDownIcon
+        className={clsx(
+          open ? "rotate-0" : "-rotate-90",
+          `h-4 w-4 ${darkBlueText}`,
+        )}
+      />
+    </button>
+  );
+}
+
+function CategoriesContainer({
+  children,
+  display,
+}: {
+  children: ReactNode;
+  display: boolean;
+}) {
+  if (display) return children;
+  return null;
+}
+
+function CategoryName({ children }: { children: ReactNode }) {
+  return children;
+}
+
+function CategoryNameContainer({ children }: { children: ReactNode }) {
+  return (
+    <div className="flex-grow basis-48 text-ellipsis whitespace-nowrap">
+      {children}
+    </div>
+  );
+}
+
+function CategoryContent({
+  children,
+}: {
+  children: (ref: RefObject<HTMLInputElement>) => ReactNode;
+}) {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleInputFocus = () => {
+    inputRef.current?.focus();
+  };
+
+  return (
+    <div
+      className={`flex items-center gap-4 py-2 pl-10 pr-2 border-b ${borderBottom} focus-within:bg-gray-100/80`}
+      onClick={handleInputFocus}
+    >
+      {children(inputRef)}
+    </div>
+  );
+}
+
+const EditAssigned = forwardRef<HTMLInputElement, { assigned: string }>(
+  ({ assigned }, ref) => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    const [value, setValue] = useState(43);
+    const [isFocused, setIsFocused] = useState(false);
+    const currency = "£";
+
+    const valueWithCurrency = `${currency} ${assigned}`;
+
+    const handleFocus = (e: React.FocusEvent<HTMLInputElement, Element>) => {
+      setIsFocused(true);
+      e.target.select();
+    };
+
+    useEffect(() => {
+      if (isFocused && inputRef.current) {
+        inputRef.current.select();
+      }
+    }, [isFocused]);
+
+    useImperativeHandle(ref, () => inputRef.current as HTMLInputElement, [
+      inputRef,
+    ]);
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+      if (e.key === "Escape") {
+        setIsFocused(false);
+        inputRef.current?.blur();
+      }
+    };
+
+    // TODO: only allow numbers in input react hook form?
+    // TODO: When clicking on the subcategory focus the assigned
+
+    return (
+      <div className="flex-1 flex justify-end p-[1px]">
+        <input
+          ref={inputRef}
+          className="w-3/4 px-1 text-right border border-transparent rounded focus:border-sky-950 hover:border-sky-950 focus:outline-none focus:ring-0 placeholder:text-black"
+          placeholder={valueWithCurrency}
+          value={isFocused ? assigned : ""}
+          onChange={(e) => setValue(+e.target.value)}
+          onFocus={handleFocus}
+          onBlur={() => setIsFocused(false)}
+          onKeyDown={handleKeyDown}
+        />
+      </div>
+    );
+  },
+);
+
+function ProgressBar({
+  assigned,
+  activity,
+  available,
+}: {
+  assigned: number;
+  activity: number;
+  available: number;
+}) {
+  // If assigned === available and activity === 0, solid green
+  //
+
+  // if assigned ===  0 full gray
+  // overspent - red on the right
+  // partially spend - hashed line until remaining/overspent
+  // fully funded full green
+
+  // const spent = (activity / assigned) * 100;
+  // let availablet = 100 - spent;
+  // if (spent > 100) {
+  //   overspent = spent - 100; // Overspent percentage
+  // } else {
+  //   overspent = 0; // No overspending
+  // }
+
+  const subCategory = {
+    spent: 33,
+    available: 33,
+    overspent: 34,
+  };
+
+  return (
+    <div className="relative h-[5px] border border-gray-400 rounded-sm">
+      <Progress className="h-full bg-gray-200 rounded" />
+      <div className="absolute top-0 left-0 h-full w-full flex rounded">
+        <Progress
+          className="h-full bg-green-200 rounded-r"
+          style={{ width: `${subCategory?.spent}%` }}
+        />
+        <Progress
+          className="h-full bg-green-400 rounded-l-[3px]  rounded-r-[3px]"
+          style={{ width: `${subCategory?.available}%` }}
+        />
+        <Progress
+          className="h-full bg-red-400 rounded-l-none rounded-r"
+          style={{ width: `${subCategory?.overspent}%` }}
+        />
+      </div>
+    </div>
+  );
+}
+
+function Activity({ children }: { children: ReactNode }) {
+  return <p className="flex-1 text-right">£{children}</p>;
+}
+
+function Available({ children }: { children: ReactNode }) {
+  return <p className="flex-1 text-right">£{children}</p>;
 }
