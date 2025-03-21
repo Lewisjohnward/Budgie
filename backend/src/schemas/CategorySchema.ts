a4313a52d1d73089b4fa124965e75fcec68b8a15
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-03-21 12:30:56 +0000

    Create categoryGroupSchema and payload

--- backend/src/schemas/CategorySchema.ts
+++ backend/src/schemas/CategorySchema.ts
@@ -11,6 +11,12 @@ export const CategorySchema = z.object({
   name: z.string().max(20, { message: "Name must be less than 20 characters" }),
 });
 
+export const CategoryGroupSchema = z.object({
+  userId: z.string().uuid(),
+  name: z.string().min(1),
+});
+
+export type CategoryGroupPayload = z.infer<typeof CategoryGroupSchema>;
 export type CategoryPayload = z.infer<typeof CategorySchema>;
 
 // TODO: NEED TO DO
