0fcef7b28c18d19be50e798c109e54513a194976
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-04-01 10:15:31 +0100

    Correctly styles account with £0

--- frontend/src/pages/budget/navBar/components/Balance.tsx
+++ frontend/src/pages/budget/navBar/components/Balance.tsx
@@ -1,8 +1,8 @@
 import { numberToCurrency } from "@/core/lib/numberToCurrency";
 
 export function Balance({ balance }: { balance: number }) {
-  const textColor = balance > 0 ? "text-white" : "text-red-600";
-  const bgColor = balance > 0 ? "bg-transparent" : "bg-white";
+  const textColor = balance >= 0 ? "text-white" : "text-red-600";
+  const bgColor = balance >= 0 ? "bg-transparent" : "bg-white";
   const formattedBalance = numberToCurrency(balance);
 
   return (
