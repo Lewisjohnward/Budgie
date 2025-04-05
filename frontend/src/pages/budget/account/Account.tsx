360d0d5cdc3dd38ad8aaeacb5358fee00528290c
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-04-05 11:51:45 +0100

    Display dropdown menu on SelectionModal

--- frontend/src/pages/budget/account/Account.tsx
+++ frontend/src/pages/budget/account/Account.tsx
@@ -72,6 +72,18 @@ import {
   DialogContent,
   DialogTitle,
 } from "@/core/components/uiLibrary/dialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/core/components/uiLibrary/dropdown-menu";
+import { DropdownMenuArrow } from "@radix-ui/react-dropdown-menu";
 
 type Category = {
   id: string;
@@ -914,6 +926,12 @@ function SelectionModal({
   display: boolean;
   cancel: () => void;
 }) {
+  const { data } = useGetCategoriesQuery();
+  if (!data) return <div>There has been an error</div>;
+
+  const categoryGroups = Object.values(data.categoryGroups);
+  const categories = data.categories;
+
   return (
     <Dialog open={display} modal={false}>
       <DialogContent
@@ -929,14 +947,65 @@ function SelectionModal({
           <X />
           {`${rowCount} Transactions`}
         </Button>
-        <Button className="bg-transparent hover:bg-white/10 focus-visible:ring-white">
-          <MdMoveToInbox />
-          Categorise
-        </Button>
-        <Button className="bg-transparent hover:bg-white/10 focus-visible:ring-white">
-          <Ellipsis />
-          More
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="bg-transparent hover:bg-white/10 focus-visible:ring-white">
+              <MdMoveToInbox />
+              Categorise
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            <DropdownMenuArrow className="fill-white" width={10} height={10} />
+            {categoryGroups.map((categoryGroup) => {
+              return (
+                <DropdownMenuGroup>
+                  <DropdownMenuGroup>
+                    <DropdownMenuSub>
+                      <DropdownMenuSubTrigger>
+                        <span>{categoryGroup.name}</span>
+                      </DropdownMenuSubTrigger>
+                      <DropdownMenuPortal>
+                        <DropdownMenuSubContent>
+                          {categoryGroup.categories.map((categoryId) => (
+                            <DropdownMenuItem>
+                              <span>{categories[categoryId].name}</span>
+                            </DropdownMenuItem>
+                          ))}
+                        </DropdownMenuSubContent>
+                      </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                  </DropdownMenuGroup>
+                </DropdownMenuGroup>
+              );
+            })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button className="bg-transparent hover:bg-white/10 focus-visible:ring-white">
+              <Ellipsis />
+              More
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-56">
+            {categoryGroups.map((categoryGroup) => {
+              return (
+                <DropdownMenuGroup>
+                  <DropdownMenuGroup>
+                    <DropdownMenuSub>
+                      <DropdownMenuSubTrigger>
+                        <span>{categoryGroup.name}</span>
+                      </DropdownMenuSubTrigger>
+                      <DropdownMenuPortal>
+                      </DropdownMenuPortal>
+                    </DropdownMenuSub>
+                  </DropdownMenuGroup>
+                </DropdownMenuGroup>
+              );
+            })}
+          </DropdownMenuContent>
+        </DropdownMenu>
       </DialogContent>
     </Dialog>
   );
