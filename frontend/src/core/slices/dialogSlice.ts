c34af53fb74c3c769b36f87554afd17ecfce53e5
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-05-04 11:48:50 +0100

    Add editingAccount field to dialogSlice

--- frontend/src/core/slices/dialogSlice.ts
+++ frontend/src/core/slices/dialogSlice.ts
@@ -1,14 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit/react";
 import { RootState } from "../store/store";
+import { Account } from "../types/NormalizedData";
 
 export type dialogState = {
   managePayees: boolean;
   editAccount: boolean;
+  editingAccount: Account | null;
 };
 
 const initialState: dialogState = {
   managePayees: false,
   editAccount: false,
+  editingAccount: null,
 };
 
 const dialogSlice = createSlice({
@@ -18,8 +21,9 @@ const dialogSlice = createSlice({
     toggleManagePayees: (state) => {
       state.managePayees = !state.managePayees;
     },
-    toggleEditAccount: (state) => {
+    toggleEditAccount: (state, action) => {
       state.editAccount = !state.editAccount;
+      state.editingAccount = action.payload;
     },
   },
 });
@@ -31,3 +35,5 @@ export const selectManagePayees = (state: RootState) =>
   state.dialogs.managePayees;
 export const selectEditAccount = (state: RootState) =>
   state.dialogs.editAccount;
+export const selectEditingAccount = (state: RootState) =>
+  state.dialogs.editingAccount;
