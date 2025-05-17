0f8ab33f677b45978013fe099e679698b086b8e9
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-05-17 16:26:38 +0100

    test: update test to account for new auth header

--- backend/src/middleware/CommonAuth.test.ts
+++ backend/src/middleware/CommonAuth.test.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { Authenticate } from "./CommonAuth";
 import { ValidateSignature } from "../utility";
+import { UserNotAuthorisedError } from "../errors";
 
 jest.mock("../utility", () => ({
   ValidateSignature: jest.fn(),
@@ -34,8 +35,11 @@ describe("CommonAuth", () => {
     expect(nextFunction).toHaveBeenCalled();
   });
 
-  test("Should return 401 with message: user not authorised", async () => {
-    mockedValidateSignature.mockResolvedValue(false);
+  test("Should call next with UserNotAuthorisedError when user not authorised", async () => {
+    const error = new UserNotAuthorisedError();
+    mockedValidateSignature.mockImplementation(() => {
+      throw error;
+    });
 
     await Authenticate(
       mockRequest as Request,
@@ -44,10 +48,16 @@ describe("CommonAuth", () => {
     );
 
     expect(ValidateSignature).toHaveBeenCalledWith(mockRequest);
-    expect(mockResponse.status).toHaveBeenCalledWith(401);
-    expect(mockResponse.json).toHaveBeenCalledWith({
-      message: "User not authorised",
-    });
-    expect(nextFunction).not.toHaveBeenCalled();
+    expect(nextFunction).toHaveBeenCalledWith(error);
+    expect(mockResponse.status).not.toHaveBeenCalled();
+    expect(mockResponse.json).not.toHaveBeenCalled();
   });
+
+  test.todo(
+    "Should call next with MissingOrMalformedAuthorizationHeaderError when missing signature",
+  );
+
+  test.todo(
+    "Should call next with MissingOrMalformedAuthorizationHeaderError when missing Bearer",
+  );
 });
