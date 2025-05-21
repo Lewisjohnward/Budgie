ab5ed35ac32856e0781c557167855f2706306cd6
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-05-21 11:20:24 +0100

    fix(test): Ensure sequential execution of integration tests

--- backend/jest.config.integration.ts
+++ backend/jest.config.integration.ts
@@ -6,6 +6,9 @@ const config: Config = {
   globalSetup: "<rootDir>/src/__test__/setup/setup.ts",
   globalTeardown: "<rootDir>/src/__test__/setup/teardown.ts",
   setupFilesAfterEnv: ["<rootDir>/src/__test__/setup/setupTests.ts"],
+  maxWorkers: 1,
+  testRunner: "jest-circus/runner",
+  verbose: true,
 };
 
 export default config;
