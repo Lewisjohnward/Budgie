77195d300a865606b524b5bc0508a50d2fbffafa
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2025-08-07 13:29:48 +0100

    feat(NavBar): add account grouping and extract grouping logic
    - Split accounts into categories (e.g. Cash, Credit)
    - Extract logic into custom hook
    - Handle open/closed state of groups in the hook

--- frontend/src/pages/budget/navBar/components/AccountOverview.tsx
+++ frontend/src/pages/budget/navBar/components/AccountOverview.tsx
@@ -1,64 +1,44 @@
 import { ChevronDownIcon } from "@/core/icons/icons";
-import { mockAccounts } from "@/mockData";
 import { AccountCard, AddAccountBtn, EmptyAccountsMessage } from ".";
-import { useGetAccountsQuery } from "@/core/api/budgetApiSlice";
 import { Balance } from "./Balance";
+import { useAccounts } from "../hooks/useAccounts";
 
-export function AccountOverview({
-  expanded,
-  toggleExpanded,
-}: {
-  expanded: boolean;
-  toggleExpanded: () => void;
-}) {
-  const { data, isLoading, isError } = useGetAccountsQuery();
-  // const accounts = mockAccounts;
+export function AccountOverview() {
+  const { userHasAccounts, accounts, balance } = useAccounts();
+
   const currency = "£";
 
   // TODO: need to handle mutiple accounts with sum of money
   // TODO: handle negative numbers
   // TODO: persist expanded close budget state when opening navbar
 
-  if (isLoading) {
-    return <div>loading</div>;
-  }
-
-  if (!data) {
-    return <div>There has been an error</div>;
-  }
-
-  const accounts = Object.values(data.accounts);
-  const balance = accounts
-    .map((account) => account.balance)
-    .reduce((sum, balance) => {
-      return sum + balance;
-    }, 0);
   return (
     <div className="space-y-2 w-60">
-      {accounts.length > 0 ? (
+      {userHasAccounts ? (
         <div className="space-y-2">
-          <button
-            onClick={toggleExpanded}
-            className="flex items-center justify-between pr-2 gap-2 w-full"
-            data-testid="expand-accounts"
-          >
-            <div className="flex items-center gap-2 tracking-wider">
-              {expanded ? (
-                // TODO: move into own component expanded prop?
-                <ChevronDownIcon />
-              ) : (
-                <ChevronDownIcon className="-rotate-90" />
+          {accounts.cash.hasAccounts && (
+            <>
+              <button
+                onClick={accounts.cash.toggleExpanded}
+                className="flex items-center justify-between pr-2 gap-2 w-full"
+                data-testid="expand-accounts"
+              >
+                <div className="flex items-center gap-2 tracking-wider">
+                  <ChevronDownIcon
+                    className={accounts.cash.expanded ? "" : "-rotate-90"}
+                  />
+                  <p className="text-sm font-[400]">CASH</p>
+                </div>
+                <Balance balance={balance} />
+              </button>
+              {accounts.cash.expanded && (
+                <div className="space-y-2">
+                  {accounts.cash.accounts.map((account) => (
+                    <AccountCard account={account} currency={currency} />
+                  ))}
+                </div>
               )}
-              <p>BUDGET</p>
-            </div>
-            <Balance balance={balance} />
-          </button>
-          {expanded && (
-            <div className="space-y-2">
-              {accounts.map((account) => (
-                <AccountCard account={account} currency={currency} />
-              ))}
-            </div>
+            </>
           )}
         </div>
       ) : (
