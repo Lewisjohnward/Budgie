c6ca604526047441918dddc699deb379dd9cebb7
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2026-07-06 14:07:54 +0100

    temp = (fe) create api types

--- frontend/src/core/types/exported-types.ts
+++ frontend/src/core/types/exported-types.ts
@@ -4,4 +4,12 @@ export type ApiBudgetSnapshot =
   paths["/budget/snapshot"]["get"]["responses"]["200"]["content"]["application/json"];
 
 export type ApiCategoryGroup = components["schemas"]["CategoryGroup"];
+export type ApiCategory = components["schemas"]["Category"];
+export type ApiMonth = components["schemas"]["Month"];
+export type ApiMemo = components["schemas"]["Memo"];
+
+export type ApiAccount = components["schemas"]["Account"];
 export type ApiPayee = components["schemas"]["Payee"];
+export type ApiTransaction = components["schemas"]["Transaction"];
+
+export type ApiMonthKey = string;
