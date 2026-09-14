d5e68add2fbf9392e42cafdaf3f5762c0327ffc1
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2026-09-14 16:31:34 +0100

    pre hash demo password and generate salt outside and provide using env variables = vps is timing out

--- backend/src/demo/application/services/seed.ts
+++ backend/src/demo/application/services/seed.ts
@@ -2,10 +2,24 @@ import { type Prisma } from "@prisma/client";
 import { createCategoryGroupWithCategories } from "../../../e2e/helpers/category";
 import { createBankAccount } from "../../../e2e/helpers/account";
 import { createNormalTransaction } from "../../../e2e/helpers/transaction";
-import { demoCredentials, registerUser } from "../../../e2e/helpers/auth";
+import { demoCredentials } from "../../../e2e/helpers/auth";
+import { authService } from "../../../features/user/auth/auth.service";
+import { categoryService } from "../../../features/budget/core/category/core/category.service";
+import { memoService } from "../../../features/budget/core/memo/memo.service";
+import { payeeService } from "../../../features/budget/core/payee/payee.service";
+import { HashedPassword, Salt } from "../../../features/user/auth/auth.types";
 
 export const seedDemo = async (tx: Prisma.TransactionClient) => {
-  const user = await registerUser(tx, demoCredentials);
+  // manually hashing password as vps is timing out
+  const user = await authService.createUser(tx, {
+    email: process.env.DEMO_EMAIL!,
+    password: process.env.DEMO_PASSWORD_HASH! as HashedPassword,
+    salt: process.env.DEMO_PASSWORD_SALT! as Salt,
+  });
+
+  await categoryService.categories.initialiseCategories(tx, user.id);
+  await memoService.initialiseMemos(tx, user.id);
+  await payeeService.initialiseSystemPayees(tx, user.id);
 
   const { categories } = await createCategoryGroupWithCategories(tx, user.id, {
     categoryGroupName: "Important",
